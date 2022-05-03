88b0e80b1d080953217c4b0e7c356384fe0f171d
unknown <samueloctavianus97@gmail.com>
2022-05-03 10:18:37 +0700

    another update

--- script.js
+++ script.js
@@ -11,13 +11,35 @@
         //if player two is currently playing, set it so that the game begins with player 1's turn.
 
 let diceImg = document.querySelector("#dice-img");
+let playerOneDiceRollScore = document.querySelector("#player-one-dice-roll-score");
+let playerOneTotalScore = document.querySelector("#player-one-total-score");
 
+//generate a random number from 1 - 6
 function generateRandomNum(){
     return Math.floor(Math.random()*6+1)
 }
 
+//starting conditions
+let totalScore = 0;
+
+
+//create a function that rolls dice & display it to the current player
 function rollDice(){
+    
+    //dice result & display
     var randomNum = generateRandomNum()
-    console.log(randomNum);
+    playerOneDiceRollScore.innerText = randomNum;
+
+    // display dice image
     diceImg.src = `./images/dice-${randomNum}.png`
+
+    // if the dice rolls 1, change turn to next player.
+    if(randomNum !== 1){
+        totalScore += randomNum;
+        playerOneTotalScore.textContent = totalScore;
+    }
+    else{
+        
+    }
+
 }
